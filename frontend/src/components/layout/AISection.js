6d5a0f07ef0fd485592e05acfb88626772d0b730
Waka.Y <104607401+waka320@users.noreply.github.com>
2025-05-04 10:00:08 +0900

    feat: advice

--- frontend/src/components/layout/AISection.js
+++ frontend/src/components/layout/AISection.js
@@ -225,7 +225,7 @@ function AISection() {
                         fontSize: isMobile ? '1.1rem' : '1.2rem'
                     }}
                 >
-                    AIからのアドバイス
+                    分析のアドバイス
                 </Typography>
                 
             </Box>
